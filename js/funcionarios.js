e2a65b4a9b511c8549112224d1f5a4d5fa560f72
Edil Queiroz de Araujo <projetorgmsp@gmail.com>
2025-06-16 00:02:19 -0300

    Add files via upload

---
+++ js/funcionarios.js
@@ -0,0 +1,11 @@
+    const funcionarios = [
+      { nome: "Alex", coords: [-23.32208, -47.42797] },
+      { nome: "Jhon", coords: [-23.36242, -51.95434] },
+      { nome: "Chuck", coords: [-25.44451, -49.27642] },
+      { nome: "Tonho", coords: [-23.15046, -46.90612] },
+      { nome: "Fred", coords: [-19.87005, -43.98925] },
+      { nome: "Jones", coords: [-29.89780, -51.26220] },
+      { nome: "Jerson", coords: [-16.55196, -49.19677] },
+      { nome: "Beto", coords: [-23.14035, -51.13037] }, 
+      { nome: "Chico", coords: [-12.08229, -38.93554] }
+    ];
